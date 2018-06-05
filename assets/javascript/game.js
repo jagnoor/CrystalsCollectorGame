c6f37dda64f7b23519e9b8e6e51444b4c18d428f
jag <jagnoor@gmail.com>
2018-06-05 18:32:52 -0500

    commit again.. WIp JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,19 +17,16 @@
 //else win if its EQUAL to random number
 
 
-    var targetNumber;
-    var lost;
-    var win;
-    var counter;
+var targetNumber = ""; // number to beat 
+var wins = 0;   // tracks the number of wins 
+var losses = 0; // tracks number of lossess 
+var counter = 0; // 
 
 
 
-    function targetNumber_tobeat () {
-		targetNumber = Math.floor(Math.random() * 102) + 19;
+    function randomTargetNumber () {
+		targetNumber = Math.floor(Math.random() * 102) + 19;  // generates a number between 19 - 120 () //
     }
-    
-
-
 
     
     function totalReset () {
@@ -40,6 +37,85 @@
     }
     
 
+    function resetCrystals () {
+		
+			var crystal ;
+			crystal.attr("value", (Math.floor(Math.random() * 12) + 1));
+			$(".card").append(crystal);
+		
+	}
 
 
 
+
+
+
+
+
+
+
+    //
+
+    // Institute Variables
+
+var targetNumber = "";
+var wins = 0;
+var losses = 0;
+var counter = 0;
+var images = ["./assets/images/Crystal-Blue.png", "./assets/images/Crystal-Purple.png", "./assets/images/Crystal-Red.png", "./assets/images/Crystal-Yellow.png"];
+
+// Functions
+
+	function randomTargetNumber () {
+		targetNumber = Math.floor(Math.random() * 102) + 19;
+	}
+
+	function resetCrystals () {
+		for (var i = 0; i < images.length; i++) {
+			var crystal = $("<img>");
+			crystal.addClass("crystal");
+			crystal.attr("src", images[i]);
+			crystal.attr("value", (Math.floor(Math.random() * 12) + 1));
+			crystal.attr("height", "100");
+			$(".crystal-images").append(crystal);
+		}
+	}
+
+	function resetHTML () {
+		$(".target-number").html(targetNumber);
+		$(".win-lose-counter").html("<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>");
+		$(".score-number").html(counter);
+		$(".crystal-images").empty();
+	}
+
+	function totalReset () {
+		randomTargetNumber ();
+		counter = 0;
+		resetHTML ();
+		resetCrystals ();
+	}
+
+// Running Code
+
+	// Inital Page Set Up
+	randomTargetNumber();
+	resetHTML ();
+	resetCrystals ();
+
+// Click Functions
+	function crystalClick () {
+		//attr returns first value of selected html element
+		counter += parseInt($(this).attr("value"));
+		$(".score-number").html(counter);
+		if (counter == targetNumber) {
+			wins++;
+			totalReset();
+		}
+		else if (counter > targetNumber) {
+			losses++;
+			totalReset();
+		};
+	};
+
+	//Throughout life cycle of the document, accounting for every single time document is dynamically changed execute crystalClick function
+	$(document).on("click", ".crystal", crystalClick);
